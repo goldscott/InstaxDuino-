a894c40ab5714deffeae93b23c1910715d29ff74
Scott Larson <goldscott@gmail.com>
2020-08-16 18:05:10 -0700

    translate comments

--- Instaduino.ino
+++ Instaduino.ino
@@ -14,7 +14,7 @@ IRrecv irrecv(broche_reception);
 decode_results decode_ir;
 LiquidCrystal_I2C  lcd(0x27, 2, 1, 0, 4, 5, 6, 7); // 0x27 is the I2C bus address for an unmodified backpack
 
-// Utilisation des E/S
+// I/O setup
 int beeper = 8;
 int shutter = 9;
 int capteur = 10;
@@ -26,27 +26,26 @@ int shutin1 = 4;
 int shutin2 = 3;
 int ShutterState;
 
-// caractères personalisés
+// custom characters
 byte bmexp[8] = {B11110, B10010, B11111, B11001, B11001, B01001, B01111,};
 byte bsand[8] = {B11111, B10001, B01110, B00100, B01010, B11111, B11111,};
 byte bflash[8] = {B00111, B01110, B01100, B00110, B01100, B11000, B10000,};
 
-// variables pour la gestion des vitesses
-// tableaux vitesses
+// Shutter speeds
 int vms[20] = {2, 4, 8, 16, 33, 66, 125, 250, 500, 1000, 2000, 4000, 6000, 8000, 10000, 12000, 15000, 20000, 30000, 45000};
 char txtvitesse[20][5] = {"500'", "250'", "125'", " 60'", " 30'", " 15'", "1/8'", "1/4'", "1/2'", "  1s", "  2s", "  4s", "  6s", "  8s", " 10s", " 12s", " 15s", " 20s", " 30s", " 45s"};
 int pvitesse = 0;
 
-// initialisation des txt variables du LCD
+// LCD init
 char ligne1[16] = "";
 char ligne2[16] = "";
 
-// initialisation options
+// init options
 boolean optmexpo = false; // option Multiexpo
 boolean optauto = true; // Option Mode Auto
 //boolean optpauset = false;
-int optsand = 0; // Option pour le retardateur
-int optflash = 0; // gestion futur du flash
+int optsand = 0; // Self timer
+int optflash = 0; // flash 
 
 
 /*------------------------------------------------------------------------*/
@@ -66,12 +65,12 @@ void setup() {
   lcd.setCursor (0, 0);
 
 
-  // activation caractères perso sur les adresses mémoire
+  // custom characters
   lcd.createChar(0, bmexp);
   lcd.createChar(1, bsand);
   lcd.createChar(2, bflash);
 
-  // activation ES
+  // init I/O
   pinMode(beeper, OUTPUT);
   pinMode(ejmotor, OUTPUT);
   pinMode(shutter, OUTPUT);
@@ -85,11 +84,11 @@ void setup() {
 
 
 
-  // BEEP de Demarrage
+  // startup beep
   delay (2000);
   beep(2);
 
-  // Lancement Ecran principal
+  // show main screen
   lcdupdate();
 
 }
@@ -107,18 +106,18 @@ void lcdupdate() {
   lcd.setCursor (0, 0);
   lcd.write(ligne1);
  
-  if (optauto)  // Ajout du texte AUTO sur la 2nd ligne
+  if (optauto)  // AUTO on second line
   {
     lcd.setCursor (1, 0);
     lcd.print("AUTO");
   }
 
-  if (optmexpo) // AJout du caractère perso Multi Expo
+  if (optmexpo) // Multi Expo character
   {
     lcd.setCursor (6, 0);
     lcd.write(byte(0));
   }
-  if (optsand != 0)  AJout des infos retardateur
+  if (optsand != 0)  //self timer info
   {
     lcd.setCursor (8, 0);
     lcd.write(byte(1));
@@ -126,7 +125,7 @@ void lcdupdate() {
     lcd.print(optsand);
   }
  
-  if (optflash != 0) AJout du caractère perso Flash
+  if (optflash != 0) //custom flash char
   {
     lcd.setCursor (12, 0);
     lcd.write(byte(2));
@@ -139,18 +138,18 @@ void lcdupdate() {
 
 // Fonction DoubleExpo simple
 void mexposhoot() {
-  lcd.setCursor (0, 1);
-  lcd.print("MultiExpo Shoot"); //On écrit le txt sur la 2nd ligne du LCD
-  digitalWrite(ejmotor, HIGH);  // On coupe le moteur via le relais 5v
+  lcd.setCursor (0, 1);         //set cursor to second line
+  lcd.print("MultiExpo Shoot");
+  digitalWrite(ejmotor, HIGH);  // stop motor via 5V relay
   delay (100);
-  digitalWrite(shutter, HIGH); // On presse le bouton déclencheur photo
+  digitalWrite(shutter, HIGH); // activate shutter
   delay (300);
-  digitalWrite(shutter, LOW); // On le relache
-  digitalWrite(capteur, HIGH); // On active le capteur d'éjection
+  digitalWrite(shutter, LOW); // release shutter
+  digitalWrite(capteur, HIGH); // activate ejection sensor
   delay (500);
-  digitalWrite(capteur, LOW);// On le relache, l'appareil pense avoir ejecté la photo
+  digitalWrite(capteur, LOW);// release ejection sensor. 
   delay (100);
-  digitalWrite(ejmotor, LOW);// On reconnecte le moteur
+  digitalWrite(ejmotor, LOW);// reconnect motor
   beep(2);
 }
 
@@ -158,15 +157,15 @@ void mexposhoot() {
 // Fonction Shoot simple
 void shoot() {
 
-  lcd.setCursor (0, 1);
-  lcd.print("AUTO SHOOT");//On écrit le txt sur la 2nd ligne du LCD
-  digitalWrite(shutter, HIGH); // On presse le bouton déclencheur photo
+  lcd.setCursor (0, 1);   //set cursor to line 2
+  lcd.print("AUTO SHOOT");
+  digitalWrite(shutter, HIGH); // activate shutter
   delay (300);
-  digitalWrite(shutter, LOW); // On le relache
+  digitalWrite(shutter, LOW); // release shutter
   beep(2);
 }
 
-// Fonction Reset Compteur de pose en simulant l'ouverture du capot. (le multiexpo déduit des photos pour rien)
+// Reset function Installation counter by simulating the opening of the cover. (the multiexpo deducted from the photos for nothing)
 void resetcpt()
 {
   beep(1);
@@ -175,12 +174,11 @@ void resetcpt()
   digitalWrite(capot, HIGH);
   delay (2000);
   digitalWrite(capot, LOW);
-  mexposhoot(); // On lance une multiexpo pour faire croire à l'appareil que le DarkSlide est sorti, le compteur passe à 10.
-
+  mexposhoot(); // We launch a multiexpo to make the device believe that the DarkSlide is out, the counter goes to 10.
 
 }
 
-// Fonction Sand/Timer
+// Self timer
 void fncsand(int timer)
 {
   for (int i = timer; i > 0; i--)
@@ -192,16 +190,14 @@ void fncsand(int timer)
     beep(1);
     delay(900);
   }
-
 }
-
 
 // Initialisation Compteur
 
-
 // BEEP standards
 void beep (int nbrebeep) {
-  for (int i = 1 ; i <= nbrebeep ; i++) {
+  for (int i = 1; i <= nbrebeep; i++)
+  {
     digitalWrite(beeper, HIGH);
     delay (50);
     digitalWrite(beeper, LOW);
@@ -231,8 +227,8 @@ void GO() {
 
 
 
-void shootmanuel(int vvms) {
-
+void shootmanuel(int vvms)
+{
   lcd.setCursor (0, 1);
   lcd.print("MANUAL SHOOT");
   digitalWrite(shutalim, HIGH);
@@ -247,12 +243,7 @@ void shootmanuel(int vvms) {
   digitalWrite(shutalim, LOW);
   delay (300);
   beep(2);
-
-
 }
-
-
-
 
 /*------------------------------------------------------------------------*/
 /*--------------------------------- LOOP ---------------------------------*/
@@ -262,7 +253,7 @@ void loop() {
   ShutterState = digitalRead(btnshutter);
   if (ShutterState == LOW) GO();
 
-  if (irrecv.decode(&decode_ir)) //Gestion des code Infrarouge reçus.
+  if (irrecv.decode(&decode_ir)) //IR decoder
   {
     if (decode_ir.value != 4294967295) {
 
@@ -286,12 +277,10 @@ void loop() {
           break;
         case 16712445://TOUCHE >>
           GO();
-
           break;
 
         case 16761405://TOUCHE Play
           optauto = !optauto;
-
           break;
 
         case 16769055: // touche -
@@ -323,6 +312,5 @@ void loop() {
     irrecv.resume(); // reçoit le prochain code
     lcdupdate();
   }
-
 }
 
